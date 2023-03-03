e5f46dd04f17a317e068a9d3c503734c237e6b2d
Phil - DopeDealers <dopedealersdev@gmail.com>
2023-03-02 21:53:40 -0500

    rr

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,8 @@
 const { models } = require('../db/connector.js');
 const renderFile = require('../utils/renderFile.js');
-const rol = require('../utils/roles');
+const roles = require('../utils/roles.js');
 
-const idToRole = require('../utils/idToRole.js');
+const idToRole = require('../utils/IdToRole.js');
 async function get(req, res) {
     //let ip = req.headers['cf-connecting-ip'] || req.headers['x-forwarded-for'] || req.realAddress || req.connection.remoteAddress,who = req.headers['user-agent'] || "Undefined (1.0.0)";
     if (!req.isAuthenticated()) renderFile(req, res, 'index.ejs', {req: req, res: res, user: undefined, isAuthenticated: false});
