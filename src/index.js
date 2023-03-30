cc95aeefbb82396d193e66031f5ffbf1f836ac85
Phil - DopeDealers <dopedealersdev@gmail.com>
2023-03-29 22:22:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
-console.log(process.env)
 require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
+var RateLimit = require('express-rate-limit');
+var MongoStore = require('rate-limit-mongo');
 const Sentry = require('@sentry/node');
 const Tracing = require("@sentry/tracing");
 const app = express();
@@ -14,9 +15,9 @@ const {
 } = require('./db/connector.js');
 const User = require('./db/User.schema.js');
 const roles = require('./utils/roles.js');
-const AdminJS = require('adminjs')
-const AdminJSExpress = require('@adminjs/express')
-AdminJS.registerAdapter(require('@adminjs/mongoose'))
+// const AdminJS = require('adminjs')
+// const AdminJSExpress = require('@adminjs/express')
+// AdminJS.registerAdapter(require('@adminjs/mongoose'))
 
 var compression = require('compression');
 const checkAuth = require('./utils/checkAuth.js');
@@ -30,30 +31,30 @@ passport.deserializeUser(function(obj, done) {
     done(null, obj);
 });
 
-// var DiscordStrategy = require('passport-discord').Strategy,
-//     refresh = require('passport-oauth2-refresh'),
-//     scopes = ['identify', 'email', 'guilds', 'guilds.join'],
-//     prompt = 'consent';
+var DiscordStrategy = require('passport-discord').Strategy,
+    refresh = require('passport-oauth2-refresh'),
+    scopes = ['identify', 'email', 'guilds', 'guilds.join'],
+    prompt = 'consent';
 
-//     console.log(process.env.DISCORD_CALLBACK_URL);
-// var discordStrat = new DiscordStrategy({
-//     clientID: process.env.DISCORD_CLIENT_ID,
-//     clientSecret: process.env.DISCORD_CLIENT_SECRET,
-//     callbackURL: process.env.DISCORD_CALLBACK_URL,
-//     scope: scopes,
-//     prompt: prompt
-// }, function(accessToken, refreshToken, profile, cb) {
-//     profile.refreshToken = refreshToken;
-//     User.findOrCreate(profile, function(err, user) {
-//         if (err) return cb(err);
-//         return cb(profile, user);
-//     });
-//     return cb(null, profile);
-// });
+    console.log(process.env.DISCORD_CALLBACK_URL);
+var discordStrat = new DiscordStrategy({
+    clientID: process.env.DISCORD_CLIENT_ID,
+    clientSecret: process.env.DISCORD_CLIENT_SECRET,
+    callbackURL: process.env.DISCORD_CALLBACK_URL,
+    scope: scopes,
+    prompt: prompt
+}, function(accessToken, refreshToken, profile, cb) {
+    profile.refreshToken = refreshToken;
+    User.findOrCreate(profile, function(err, user) {
+        if (err) return cb(err);
+        return cb(profile, user);
+    });
+    return cb(null, profile);
+});
 
 function middleWaresOrSets() {
-    // passport.use(discordStrat);
-    // refresh.use(discordStrat);
+    passport.use(discordStrat);
+    refresh.use(discordStrat);
     app.use(compression());
     app.use(session({
         secret: 'secret',
@@ -86,25 +87,25 @@ function routes() {
     middleWaresOrSets();
     app.get('/', routesArray[0].get.bind(this));
     app.get('/pricing', routesArray[1].get.bind(this));
-    // app.get('/api/v1/login', passport.authenticate('discord', {
-    //     scope: scopes,
-    //     prompt: prompt
-    // }));
-    // app.get('/api/v1/callback',
-    //     passport.authenticate('discord', {
-    //         failureRedirect: '/'
-    //     }),
-    //     function(req, res) {
-    //         res.redirect('/')
-    //     });
-    // app.get('/api/v1/logout', function(req, res) {
-    //     req.logout(function(err) {
-    //         if (err) {
-    //             return next(err);
-    //         }
-    //         res.redirect('/');
-    //     });
-    //});
+    app.get('/api/v1/login', passport.authenticate('discord', {
+        scope: scopes,
+        prompt: prompt
+    }));
+    app.get('/api/v1/callback',
+        passport.authenticate('discord', {
+            failureRedirect: '/'
+        }),
+        function(req, res) {
+            res.redirect('/')
+        });
+    app.get('/api/v1/logout', function(req, res) {
+        req.logout(function(err) {
+            if (err) {
+                return next(err);
+            }
+            res.redirect('/');
+        });
+    });
 
     connectDb().then(async (errMongo) => {
         // const AdminPanel = new AdminJS({
@@ -114,10 +115,37 @@ function routes() {
         //             actions: {
         //                 delete: {
         //                     guard: "Are you sure you wish to delete this record?"
+        //                 },
+        //                 regenerateToken: {
+        //                     actionType: 'record',
+        //                     icon: 'View',
+        //                     isVisible: true,
+        //                     component: './adminJsComponents/generateApiComp.jsx',
+        //                     handler: async (req, res, context) => {
+        //                         const user = context.record;
+        //                         const UserAc = context._admin.findResource('UserAccount')
+        //                         const crypto = require('crypto');
+        //                         user.param('api_token').value = crypto.randomBytes(32).toString('hex');
+        //                         return {
+        //                             record: user.toJSON(context.currentAdmin)
+        //                         }
+        //                     }
         //                 }
-        //             }
+        //             },
+        //             properties: {
+        //                 api_token: {
+        //                     type: 'string',
+        //                     isVisible: {
+        //                         list: true,
+        //                         edit: true,
+        //                         filter: false,
+        //                         show: false,
+        //                     },
+        //                 },
+        //             },
+
         //         }
-        //     }],
+        //     }, ],
         //     branding: {
         //         companyName: 'Cyci Org',
         //         logo: 'https://cdn.cyci.rocks/576688747481743/22613_CyciRocks_Rainbowsvg.svg',
@@ -127,7 +155,7 @@ function routes() {
         //     rootPath: '/admin',
         // })
         // const router = AdminJSExpress.buildRouter(AdminPanel, checkAuthPlusAdmin);
-        //app.use(AdminPanel.options.rootPath, router)
+        // app.use(AdminPanel.options.rootPath, router)
         app.listen(process.env.PORT, function(err) {
           
             if (err) return console.log(err)
